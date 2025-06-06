ec4cc723824329464028d230d889db94e9f2884c
maestre-michaelrua <maestremichaelrua@gmail.com>
2025-06-06 15:42:21 +0800

    add mike name

--- main.py
+++ main.py
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
-print("git branch demo")
-print("this change is from fetaure-1")
-print("this is chamges from main branch")
-=======
 print("My name is Vince Adrian A. Besa")
->>>>>>> d1aa48e0f7b01bc0b3319d94cba45a9b7d24a3d7
+print("i am mikesyikes")
