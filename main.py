549aca968125ece2b5225db409c467942658081b
salespara-ricagenevive <rgslspr14@gmail.com>
2025-06-06 16:27:52 +0800

    change salespara name

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 print("My name is Vince Adrian A. Besa")
 print("My name is Michael Rua S. Maestre")
 print("My name is Zcintilla R. Serquina")
-print("My name is ica")
+print("My name is Rica Genevive B. Salespara")
